f449cad579788c5158c289d026e7de32465200d1
Amir <amir@tvmc.org.uk>
2026-03-16 10:49:04 +0000

    Fix prod formatting: remove Tailwind CDN, restore local Lucide

--- app/views/layout.php
+++ app/views/layout.php
@@ -17,15 +17,8 @@
         })();
     </script>
     <title><?php echo isset($pageTitle) ? sanitize($pageTitle) . ' - ' : ''; ?>Prayer Times - UK Mosques</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script>
-        tailwind.config = {
-            darkMode: 'class',
-            corePlugins: { preflight: false }
-        };
-    </script>
-    <script src="https://unpkg.com/lucide@0.542.0/dist/umd/lucide.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="/js/lucide.min.js"></script>
     <link rel="stylesheet" href="/css/style.css">
     <style>
         body {
